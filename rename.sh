68674e0619588eaa51fe2a3b064d4e62e40aa822
brian <mitukab@outlook.com>
2019-09-08 14:57:41 +0300

    Added checks for pdfgrep and pdf files

---
+++ rename.sh
@@ -0,0 +1,18 @@
+
+##check if pdfGrep is installed and there are pdf files in the directory
+if ! [ -x "$(command -v pdfgrep)" ]; then
+  echo 'Error: pdfgrep is not installed download it to continue' >&2
+  exit 1
+  else
+  echo 'pdfgrep installed proceeding to search pdfs' 
+fi
+
+count=`ls -1 *.pdf 2>/dev/null | wc -l`
+
+if [ $count = 0 ]; then
+    echo "No pdf in the directory exiting....."
+    exit 1
+    else 
+    echo "Pdfs found in the directory proceeding to rename"
+fi
+
